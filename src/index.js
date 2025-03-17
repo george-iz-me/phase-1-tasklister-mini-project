815e140f785d4c625f2e4025009d10d99875bc12
George <george.njogu@moringaschool.com>
2025-03-17 16:31:00 +0300

    test assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  let form = document.getElementById('create-task-form')
+  let tasks = document.getElementById("tasks")
+  
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    const li = document.createElement('li')
+    const taskInput = document.getElementById('new-task-description')
+    li.textContent = taskInput.value + " "
+    const btn = document.createElement('button')
+    btn.textContent = 'x'
+    li.appendChild(btn)
+    tasks.appendChild(li)
+    btn.addEventListener('click', handleDelete)
+  })
+
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  }
+  
+});
+document.addEventListener("DOMContentLoaded", () => {
+  
+  let form = document.getElementById('create-task-form')
+  let tasks = document.getElementById("tasks")
+  
+  form.addEventListener('submit', (e)=> {
+    e.preventDefault()
+    const li = document.createElement('li')
+    const taskInput = document.getElementById('new-task-description')
+    li.textContent = taskInput.value + " "
+    const btn = document.createElement('button')
+    btn.textContent = 'x'
+    li.appendChild(btn)
+    tasks.appendChild(li)
+    btn.addEventListener('click', handleDelete)
+  })
+
+  function handleDelete(e){
+    e.target.parentNode.remove()
+  }
+  
 });
